44cdc45c0317aa27475e9f1465b071bca0e8025d
Maskeit <miguelagustin182@gmail.com>
2023-05-25 00:37:12 -0600

    Clase de mañana

--- pythonGPT/config.py
+++ pythonGPT/config.py
@@ -3,16 +3,16 @@
 
 #IMPORTANTE INSTALAR LOS REQUERIMIENTOS DEL ARCHIVO "requirements.txt" con $  pip install requierements.txt
 #Llave de la api generada
-api_key = "sk-llave"
+api_key = "sk-"
 
 #Roles
 """
 openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "system", "content": "You are a helpful assistant."}, el system maneja el contexto
+        {"role": "user", "content": "Who won the world series in 2020?"}, nosotros
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."}, guarda el contexto
         {"role": "user", "content": "Where was it played?"}
     ]
 )
